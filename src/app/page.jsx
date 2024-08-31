ded74b405ff01aa27b96b348770d25e8d64519c6
mohammadfarzaan <farzanmohd2286@gmail.com>
2024-08-31 16:41:32 +0530

    refining changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -185,7 +185,7 @@ const Home = () => {
 
   <>
   {/* Testimonials */}
-  <div className="relative h-[500px] mt-10 z-10 overflow-hidden">
+  <div className="relative h-[550px] mt-10 z-10 overflow-hidden">
     <div className="max-w-[85rem] px-4 py-12 sm:px-6 lg:px-8 lg:py-16 mx-auto">
       {/* Gradients */}
       <div aria-hidden="true" className="flex -z-[1] absolute start-0">
@@ -309,102 +309,6 @@ const Home = () => {
           </h3>
           <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
             Support Consultant
-          </p>
-        </div>
-      </div>
-      {/* End Col */}
-      <div className="text-center">
-        <img
-          className="rounded-xl sm:size-48 lg:size-60 mx-auto"
-          src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-          alt="Avatar"
-        />
-        <div className="mt-2 sm:mt-4">
-          <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-            Maria Powers
-          </h3>
-          <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-            Director of sales
-          </p>
-        </div>
-      </div>
-      {/* End Col */}
-      <div className="text-center">
-        <img
-          className="rounded-xl sm:size-48 lg:size-60 mx-auto"
-          src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-          alt="Avatar"
-        />
-        <div className="mt-2 sm:mt-4">
-          <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-            Delia Pawelke
-          </h3>
-          <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-            Front-end Developer
-          </p>
-        </div>
-      </div>
-      {/* End Col */}
-      <div className="text-center">
-        <img
-          className="rounded-xl sm:size-48 lg:size-60 mx-auto"
-          src="https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-          alt="Avatar"
-        />
-        <div className="mt-2 sm:mt-4">
-          <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-            Tom Lowry
-          </h3>
-          <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-            UI/UX Designer
-          </p>
-        </div>
-      </div>
-      {/* End Col */}
-      <div className="text-center">
-        <img
-          className="rounded-xl sm:size-48 lg:size-60 mx-auto"
-          src="https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-          alt="Avatar"
-        />
-        <div className="mt-2 sm:mt-4">
-          <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-            Louise Donadieu
-          </h3>
-          <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-            Support Consultant
-          </p>
-        </div>
-      </div>
-      {/* End Col */}
-      <div className="text-center">
-        <img
-          className="rounded-xl sm:size-48 lg:size-60 mx-auto"
-          src="https://images.unsplash.com/photo-1514222709107-a180c68d72b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-          alt="Avatar"
-        />
-        <div className="mt-2 sm:mt-4">
-          <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-            Jeff Fisher
-          </h3>
-          <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-            Project Manager
-          </p>
-        </div>
-      </div>
-      {/* End Col */}
-      <div className="text-center">
-        <img
-          className="rounded-xl sm:size-48 lg:size-60 mx-auto"
-          src="https://images.unsplash.com/photo-1602452920335-6a132309c7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-          alt="Avatar"
-        />
-        <div className="mt-2 sm:mt-4">
-          <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-            Sophia Harrington
-          </h3>
-          <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-            Project Manager
           </p>
         </div>
       </div>
